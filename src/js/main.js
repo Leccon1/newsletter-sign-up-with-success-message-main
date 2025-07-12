6a0f56b4e245f316cb9faa3db6ae4225aae8c380
Leccon1 <dlavania703@gmail.com>
2025-07-13 02:52:53 +0300

    js logic is ready

---
+++ src/js/main.js
@@ -0,0 +1,42 @@
+const form = document.getElementById('form')
+const closeBtn = document.getElementById('closeBtn')
+const userInput = document.getElementById('email')
+const errorParagraph = document.getElementById('textError')
+const card = document.getElementById('card')
+const cardSuccess = document.getElementById('cardSuccess')
+const userEmail = document.getElementById('userEmail')
+let checkError = false
+
+form.addEventListener('submit', function (e) {
+	e.preventDefault()
+
+	const email = userInput.value.trim()
+	const isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+
+	if (!isValid && checkError === false) {
+		userInput.classList.add('error')
+		errorParagraph.style.display = 'block'
+		errorParagraph.style.opacity = '1'
+		errorParagraph.style.visibility = 'visible'
+	} else {
+		checkError = true
+		userInput.classList.remove('error')
+		card.style.display = 'none'
+		cardSuccess.style.display = 'block'
+		userEmail.textContent = email
+	}
+	console.log(checkError)
+})
+
+userInput.addEventListener('click', () => {
+	if (checkError === false) {
+		userInput.classList.remove('error')
+		errorParagraph.style.visibility = 'hidden'
+		errorParagraph.style.opacity = '0'
+	}
+})
+
+closeBtn.addEventListener('click', () => {
+	cardSuccess.style.display = 'none'
+	card.style.display = 'flex'
+})
